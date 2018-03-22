bc9a41f8288d42fdb45e76e3baae9aacfeac5107
ttupper92618 <ttupper@uxdesignassoaciates.com>
2018-03-22 15:45:47 -0700

    Initial source commit

---
+++ AwpLocationEngine.framework/Headers/AwpLocationEngine.h
@@ -0,0 +1,19 @@
+//
+//  AwpLocationEngine.h
+//  AwpLocationEngine
+//
+//  Created by Daniel Wu on 5/10/16.
+//  Copyright © 2016 Awarepoint. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AwpLocationEngine.
+FOUNDATION_EXPORT double AwpLocationEngineVersionNumber;
+
+//! Project version string for AwpLocationEngine.
+FOUNDATION_EXPORT const unsigned char AwpLocationEngineVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AwpLocationEngine/PublicHeader.h>
+
+
---
+++ Pods/Target Support Files/Pods-AwareHealth/Pods-AwareHealth-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_AwareHealth : NSObject
+@end
+@implementation PodsDummy_Pods_AwareHealth
+@end
